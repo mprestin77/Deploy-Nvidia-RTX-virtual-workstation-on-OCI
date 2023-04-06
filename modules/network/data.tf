c89f547110ed74355d6612ad97ffb092da853144
mprestin77 <mprestin@gmail.com>
2023-04-06 09:07:39 -0700

    Delete modules directory

--- modules/network/data.tf
+++
@@ -1,8 +0,0 @@
-data "oci_core_services" "net_services" {
-#  count = var.use_existing_vnc ? 0 : 1
-  filter {
-    name   = "name"
-    values = ["All .* Services In Oracle Services Network"]
-    regex  = true
-  }
-}
