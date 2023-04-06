cd79075ab8e2f1a47ce32f2aca8e9f896c2b4f69
mprestin77 <mprestin@gmail.com>
2023-04-06 09:07:18 -0700

    Delete variables.tf

--- modules/bastion/variables.tf
+++
@@ -1,8 +0,0 @@
-# ---------------------------------------------------------------------------------------------------------------------
-# Environmental variables
-# You probably want to define these as environmental variables.
-# Instructions on that are here: https://github.com/oci-quickstart/oci-prerequisites
-# ---------------------------------------------------------------------------------------------------------------------
-
-variable "compartment_ocid" {}
-variable "subnet_id" {}
